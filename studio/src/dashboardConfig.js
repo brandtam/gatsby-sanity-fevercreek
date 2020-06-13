c8cb679c357b73eccc78197b723a7750abb3f2f0
Brandt Milczewski <brandt@shopdevs.com>
2020-06-12 20:05:18 -0700

    setup Netlify deploy

---
+++ studio/src/dashboardConfig.js
@@ -0,0 +1,24 @@
+export default {
+  widgets: [
+      {
+      name: 'netlify',
+      options: {
+        title: 'My Netlify deploys',
+        sites: [
+          {
+            title: 'Sanity Studio',
+            apiId: 'pyw01he5',
+            buildHookId: '5ee4423356089a5d0a12d6e4',
+            name: 'gatsby-sanity-fevercreek-studio',
+          },
+          {
+            title: 'Website',
+            apiId: 'yyyyy-xxxxx-zzzz-xxxx-yyyyyyyy',
+            buildHookId: 'yyyyxxxxxyyyxxdxxx',
+            name: 'gatsby-sanity-fevercreek'
+          }
+        ]
+      }
+    }
+  ]
+}
